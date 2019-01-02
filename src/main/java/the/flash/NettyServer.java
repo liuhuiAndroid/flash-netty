27712d1eabf17020cac5d74363f4c653ffabfc52
liu hui <im.lh@hotmail.com>
2019-01-02 22:05:50 +0800

    Netty 学习

--- src/main/java/the/flash/NettyServer.java
+++ src/main/java/the/flash/NettyServer.java
@@ -10,28 +10,38 @@ import io.netty.util.AttributeKey;
 
 public class NettyServer {
 
+    // 起始端口号
     private static final int BEGIN_PORT = 8000;
 
     public static void main(String[] args) {
+        // bossGroup表示监听端口，accept 新连接的线程组
         NioEventLoopGroup boosGroup = new NioEventLoopGroup();
+        // workerGroup表示处理每一条连接的数据读写的线程组
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
 
+        // 引导类 ServerBootstrap，引导我们进行服务端的启动工作
         final ServerBootstrap serverBootstrap = new ServerBootstrap();
         final AttributeKey<Object> clientKey = AttributeKey.newInstance("clientKey");
         serverBootstrap
+                // 给引导类配置两大线程组
                 .group(boosGroup, workerGroup)
+                // 指定我们服务端的 IO 模型为NIO
                 .channel(NioServerSocketChannel.class)
+                // 给NioServerSocketChannel指定一些自定义属性
                 .attr(AttributeKey.newInstance("serverName"), "nettyServer")
+                // 给每一条连接指定自定义属性
                 .childAttr(clientKey, "clientValue")
+                // 系统用于临时存放已完成三次握手的请求的队列的最大长度
                 .option(ChannelOption.SO_BACKLOG, 1024)
+                // 给每条连接设置一些TCP底层相关的属性
                 .childOption(ChannelOption.SO_KEEPALIVE, true)
                 .childOption(ChannelOption.TCP_NODELAY, true)
+                // 定义后续每条连接的数据读写，业务处理逻辑
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     protected void initChannel(NioSocketChannel ch) {
                         System.out.println(ch.attr(clientKey).get());
                     }
                 });
-
 
         bind(serverBootstrap, BEGIN_PORT);
     }
