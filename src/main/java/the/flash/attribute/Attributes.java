df7347631836594796a7e5335d8dadd573f9b092
liu hui <im.lh@hotmail.com>
2019-01-03 16:50:42 +0800

    Netty 学习

--- src/main/java/the/flash/attribute/Attributes.java
+++ src/main/java/the/flash/attribute/Attributes.java
@@ -3,5 +3,7 @@ package the.flash.attribute;
 import io.netty.util.AttributeKey;
 
 public interface Attributes {
+
     AttributeKey<Boolean> LOGIN = AttributeKey.newInstance("login");
+
 }
--- src/main/java/the/flash/util/LoginUtil.java
+++ src/main/java/the/flash/util/LoginUtil.java
@@ -5,13 +5,25 @@ import io.netty.util.Attribute;
 import the.flash.attribute.Attributes;
 
 public class LoginUtil {
+
+    /**
+     * 设置登录标志位
+     *
+     * @param channel
+     */
     public static void markAsLogin(Channel channel) {
         channel.attr(Attributes.LOGIN).set(true);
     }
 
+    /**
+     * 判断是否有标志位
+     *
+     * @param channel
+     * @return
+     */
     public static boolean hasLogin(Channel channel) {
         Attribute<Boolean> loginAttr = channel.attr(Attributes.LOGIN);
-
         return loginAttr.get() != null;
     }
+
 }
